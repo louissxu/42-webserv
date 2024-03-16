b9cc56ff8c650e0dd278e9d99d8ff0cff52aa629
Mehdi Mirzaie <mehdimirzaie18@gmail.com>
2024-03-16 14:21:31 +1030

    remove merge conflict in cgi-bin

--- application/cgi-bin/login.py
+++ application/cgi-bin/login.py
@@ -1,64 +1,3 @@
-<<<<<<< HEAD
-import os
-import user
-
-try:
-	body = input()
-except EOFError:
-	print("Login.py read error\n")
-	exit()
-
-if (body == ""):
-	exit()
-
-username, password = body.split('&')
-username = username.removeprefix("username=")
-password = password.removeprefix("password=")
-
-# session_id = os.getenv("session_id")
-# if (session_id == None):
-# 	session_id = 12345
-
-oldUser = user.User(0, username, password, "")
-if (oldUser.getUserByUsername(username) == True):
-	print("<!DOCTYPE html>")
-	print("<html>")
-	print("<head>")
-	print("<title>Form Submission Result</title>")
-	print("<style>h1{ text-align: center; }pre{ text-align: center; }</style>")
-	print("</head>")
-	print("<body>")
-	print("<h1>Form Submission Result</h1>")
-	print("<pre>welcome back: " + username + "</pre>")
-	print("</body>")
-	print("</html>")
-else:
-	print("<!DOCTYPE html>")
-	print("<html>")
-	print("<head>")
-	print("<title>Form Submission Result</title>")
-	print("<style>h1{ text-align: center; }pre{ text-align: center; }</style>")
-	print("</head>")
-	print("<body>")
-	print("<h1>Form Submission Result</h1>")
-	print("<pre>no such user found: " + username + "</pre>")
-	print("</body>")
-	print("</html>")
-
-
-
-
-# session_id = os.getenv("session_id")
-# newUser = user.User(session_id, username, password, firstname)
-
-# print ("HTTP/1.1 200 OK")
-# print ("Connection: Keep-Alive")
-# print ("Content-Length: 221")
-# print ("Content-Type: text/html")
-# print ("Server: mehdi's_webserv")
-# print
-
-=======
 import cgi
 import user
 import os
@@ -125,4 +64,3 @@ print("<input type='password' placeholder='Enter Password' name='password' requi
 print("<input type='submit' value='Submit'/>")
 print("</form>")
 print("</body></html>")
->>>>>>> origin/main
--- application/cgi-bin/register.py
+++ application/cgi-bin/register.py
@@ -1,64 +1,5 @@
 import os
 import user
-<<<<<<< HEAD
-import sqlite3
-
-try:
-	body = input()
-except EOFError:
-	print("hello")
-	exit()
-
-if (body == ""):
-	exit()
-
-username, password, firstname = body.split('&')
-username = username.removeprefix("username=")
-password = password.removeprefix("password=")
-firstname = firstname.removeprefix("firstname=")
-
-session_id = os.getenv("session_id")
-if (session_id == None):
-	session_id = 12345
-# print (session_id)
-newUser = user.User(session_id, username, password, firstname)
-
-if (newUser.getUserByUsername(username) == False):
-	newUser.addUser()
-	print("<!DOCTYPE html>")
-	print("<html>")
-	print("<head>")
-	print("<title>Form Submission Result</title>")
-	print("<style>h1{ text-align: center; }pre{ text-align: center; }</style>")
-	print("</head>")
-	print("<body>")
-	print("<h1>Form Submission Result</h1>")
-	print("<pre>thanks for registering: " + firstname + "</pre>")
-	print("</body>")
-	print("</html>")
-else:
-	print("<!DOCTYPE html>")
-	print("<html>")
-	print("<head>")
-	print("<title>Form Submission Result</title>")
-	print("<style>h1{ text-align: center; }pre{ text-align: center; }</style>")
-	print("</head>")
-	print("<body>")
-	print("<h1>Form Submission Result</h1>")
-	print("<pre>welcome back you are already a member: " + firstname + "</pre>")
-	print("</body>")
-	print("</html>")
-
-
-
-# print ("HTTP/1.1 200 OK")
-# print ("Connection: Keep-Alive")
-# print ("Content-Length: 221")
-# print ("Content-Type: text/html")
-# print ("Server: mehdi's_webserv")
-# print 
-
-=======
 import cgi
 
 print()
@@ -90,4 +31,3 @@ if "username" in form and "password" in form:
 
 print("</body>")
 print("</html>")
->>>>>>> origin/main
